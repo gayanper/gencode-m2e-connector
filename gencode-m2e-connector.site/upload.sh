a2b9765d485e9086b61c78e9b9c4a57e24e47898
Gayan Perera <gayanper@gmail.com>
2017-12-03 22:48:05 +0530

    Updated the upload.sh file for the plugin

--- gencode-m2e-connector.site/upload.sh
+++ gencode-m2e-connector.site/upload.sh
@@ -4,6 +4,6 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-plugins-extras/$VERSION/eclipse-plugins-extras/$VERSION/archive-site.zip?explode=1
-curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-plugins-extras/$VERSION/publish
+curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gencode-m2e-connector/$VERSION/gencode-m2e-connector/$VERSION/archive-site.zip?explode=1
+curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gencode-m2e-connector/$VERSION/publish
 
