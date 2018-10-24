9e25d095cb398d5fcdb663cf0129017dcb3104eb
gayanper <gayanper@gmail.com>
2018-10-24 11:04:36 +0530

    update upload script

--- gencode-m2e-connector.site/upload.sh
+++ gencode-m2e-connector.site/upload.sh
@@ -4,6 +4,11 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gencode-m2e-connector/$VERSION/gencode-m2e-connector/$VERSION/archive-site.zip?explode=1
+curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
+        -H "X-Bintray-Package:gencode-m2e-connector" \
+        -H "X-Bintray-Version:$VERSION" \
+        -H "X-Bintray-Explode:1" \
+        https://api.bintray.com/content/gayanper/p2/gencode-m2e-connector/$VERSION
+
 curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gencode-m2e-connector/$VERSION/publish
 
